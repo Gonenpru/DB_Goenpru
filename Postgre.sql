9cced8f0b58240cb8659ce0f1a646931b911eabd
Sampru <sampru_96@hotmail.com>
2017-01-05 11:19:13 +0100

    Semicolon added and Tickets corrected.

--- Postgre.sql
+++ Postgre.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS Passengers (
   surname VARCHAR(45) NULL,
   email VARCHAR(64) NULL,
   password VARCHAR(64) NULL,
-  PRIMARY KEY (id))
+  PRIMARY KEY (id));
 
   CREATE TABLE IF NOT EXISTS Controllers (
   id SERIAL,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS Passengers (
   surname VARCHAR(45) NULL,
   email VARCHAR(64) NULL,
   password VARCHAR(64) NULL,
-  PRIMARY KEY (id))
+  PRIMARY KEY (id));
 
 
   CREATE TABLE IF NOT EXISTS Airlines (
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS Passengers (
   name VARCHAR(45) NULL,
   email VARCHAR(45) NULL,
   password VARCHAR(64) NULL,
-  PRIMARY KEY (id))
+  PRIMARY KEY (id));
 
 
   CREATE TABLE IF NOT EXISTS Employees (
@@ -29,12 +29,12 @@ CREATE TABLE IF NOT EXISTS Passengers (
   surname VARCHAR(45) NULL,
   email VARCHAR(45) NULL,
   password VARCHAR(64) NULL,
-  PRIMARY KEY (id))
+  PRIMARY KEY (id));
 
   CREATE TABLE IF NOT EXISTS Manufacturers (
   id SERIAL,
   name VARCHAR(45) NULL,
-  PRIMARY KEY (id))
+  PRIMARY KEY (id));
 
 CREATE TABLE IF NOT EXISTS Plane_Models (
   id SERIAL,
@@ -46,15 +46,14 @@ CREATE TABLE IF NOT EXISTS Plane_Models (
     FOREIGN KEY (Manufacturer_id)
     REFERENCES Manufacturers (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
- ;
+    ON UPDATE NO ACTION);
 
 CREATE INDEX fk_Plane_Models_Manufacturers_idx ON Plane_Models (Manufacturer_id ASC);
 
 CREATE TABLE IF NOT EXISTS Plane_Status (
   id SERIAL,
   name VARCHAR(45) NULL,
-  PRIMARY KEY (id))
+  PRIMARY KEY (id));
 
 
 CREATE TABLE IF NOT EXISTS Planes (
@@ -62,8 +61,7 @@ CREATE TABLE IF NOT EXISTS Planes (
   Plane_Model_id INT NOT NULL,
   Plane_Status_id INT NOT NULL,
   Airline_id INT NOT NULL,
-  PRIMARY KEY (id)
- ,
+  PRIMARY KEY (id),
   CONSTRAINT fk_Planes_Plane_Models1
     FOREIGN KEY (Plane_Model_id)
     REFERENCES Plane_Models (id)
@@ -78,8 +76,7 @@ CREATE TABLE IF NOT EXISTS Planes (
     FOREIGN KEY (Airline_id)
     REFERENCES Airlines (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-;
+    ON UPDATE NO ACTION);
 
 CREATE INDEX fk_Planes_Plane_Models1_idx ON Planes (Plane_Model_id ASC);
 CREATE INDEX fk_Planes_Plane_Status1_idx ON Planes (Plane_Status_id ASC);
@@ -92,14 +89,12 @@ CREATE TABLE IF NOT EXISTS Plane_Movements (
   dirX INT NULL,
   dirY INT NULL,
   Plane_id INT NOT NULL,
-  PRIMARY KEY (id)
- ,
+  PRIMARY KEY (id) ,
   CONSTRAINT fk_Plane_Movements_Planes1
     FOREIGN KEY (Plane_id)
     REFERENCES Planes (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
- ;
+    ON UPDATE NO ACTION);
 
 CREATE INDEX fk_Plane_Movements_Planes1_idx ON Plane_Movements (Plane_id ASC);
 
@@ -109,7 +104,7 @@ CREATE INDEX fk_Plane_Movements_Planes1_idx ON Plane_Movements (Plane_id ASC);
 CREATE TABLE IF NOT EXISTS Terminals (
   id SERIAL,
   name VARCHAR(45) NULL,
-  PRIMARY KEY (id))
+  PRIMARY KEY (id));
 
 
 
@@ -117,14 +112,12 @@ CREATE TABLE IF NOT EXISTS Gates (
   id SERIAL,
   name VARCHAR(45) NULL,
   Terminal_id INT NOT NULL,
-  PRIMARY KEY (id)
- ,
+  PRIMARY KEY (id),
   CONSTRAINT fk_Gates_Terminals1
     FOREIGN KEY (Terminal_id)
     REFERENCES Terminals (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
- ;
+    ON UPDATE NO ACTION);
 
 CREATE INDEX fk_Gates_Terminals1_idx ON Gates (Terminal_id ASC);
 
@@ -132,14 +125,12 @@ CREATE TABLE IF NOT EXISTS Baggages (
   id SERIAL,
   name VARCHAR(45) NULL,
   Terminal_id INT NOT NULL,
-  PRIMARY KEY (id)
- ,
+  PRIMARY KEY (id),
   CONSTRAINT fk_Baggages_Terminals1
     FOREIGN KEY (Terminal_id)
     REFERENCES Terminals (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
- ;
+    ON UPDATE NO ACTION);
 
 CREATE INDEX fk_Baggages_Terminals1_idx ON Baggages (Terminal_id ASC);
 
@@ -150,14 +141,12 @@ CREATE TABLE IF NOT EXISTS Routes (
   departure VARCHAR(45) NULL,
   arrival VARCHAR(45) NULL,
   Airline_id INT NOT NULL,
-  PRIMARY KEY (id)
- ,
+  PRIMARY KEY (id),
   CONSTRAINT fk_Routes_Airlines1
     FOREIGN KEY (Airline_id)
     REFERENCES Airlines (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
- ;
+    ON UPDATE NO ACTION);
 
 CREATE INDEX fk_Routes_Airlines1_idx ON Routes (Airline_id ASC);
 
@@ -171,8 +160,7 @@ CREATE TABLE IF NOT EXISTS Flights (
   Plane_id INT NOT NULL,
   Gate_id INT NOT NULL,
   Baggage_id INT NOT NULL,
-  PRIMARY KEY (id)
- ,
+  PRIMARY KEY (id),
   CONSTRAINT fk_Flights_Routes1
     FOREIGN KEY (Route_id)
     REFERENCES Routes (id)
@@ -192,35 +180,29 @@ CREATE TABLE IF NOT EXISTS Flights (
     FOREIGN KEY (Baggage_id)
     REFERENCES Baggages (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
- ;
+    ON UPDATE NO ACTION);
 
 CREATE INDEX fk_Flights_Routes1_idx ON Flights (Route_id ASC);
 CREATE INDEX fk_Flights_Planes1_idx ON Flights (Plane_id ASC);
 CREATE INDEX fk_Flights_Gates1_idx ON Flights (Gate_id ASC);
 CREATE INDEX fk_Flights_Baggages1_idx ON Flights (Baggage_id ASC);
 
-
-CREATE SEQUENCE gonenpru.Tickets_seq;
-
-CREATE TABLE IF NOT EXISTS gonenpru.Tickets (
-  id INT NOT NULL DEFAULT NEXTVAL ('gonenpru.Tickets_seq'),
+CREATE TABLE IF NOT EXISTS Tickets (
+  id SERIAL,
   code VARCHAR(45) NULL,
   Flight_id INT NOT NULL,
   Passenger_id INT NOT NULL,
-  PRIMARY KEY (id)
- ,
+  PRIMARY KEY (id),
   CONSTRAINT fk_Tickets_Flights1
     FOREIGN KEY (Flight_id)
-    REFERENCES gonenpru.Flights (id)
+    REFERENCES Flights (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_Tickets_Passengers1
     FOREIGN KEY (Passenger_id)
-    REFERENCES gonenpru.Passengers (id)
+    REFERENCES Passengers (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
- ;
+    ON UPDATE NO ACTION);
 
-CREATE INDEX fk_Tickets_Flights1_idx ON gonenpru.Tickets (Flight_id ASC);
-CREATE INDEX fk_Tickets_Passengers1_idx ON gonenpru.Tickets (Passenger_id ASC);
+CREATE INDEX fk_Tickets_Flights1_idx ON Tickets (Flight_id ASC);
+CREATE INDEX fk_Tickets_Passengers1_idx ON Tickets (Passenger_id ASC);
